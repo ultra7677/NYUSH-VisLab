c6d6d40b9de86aed7a0dc9d4fe4d3073596311dc
ultra7677 <ultra7677@gmail.com>
2016-07-20 01:54:07 +0800

    upload UI-Design Document

--- public/javascripts/directives/hexagon.js
+++ public/javascripts/directives/hexagon.js
@@ -7,8 +7,13 @@ angular.module('demoApp').directive("hexagon", function($window) {
         restrict: "EA",
         template: "<div id='chart'></div>",
         link: function(scope, elem, attrs){
-           // console.log(hexagon);
-            hexagon.init();
+
+            var canvasContainer = 'chart';
+
+            hexagon
+                .size([$(window).width()*0.95,800])
+                .init(canvasContainer)
+                .render();
         }
     };
 });
--- public/javascripts/visualization/hexagon.js
+++ public/javascripts/visualization/hexagon.js
@@ -3,7 +3,9 @@
  */
 hexagon = function(){
     var hexagon = {},
-        size = [960, 800];
+        size = [,];
+
+    var camera,scene,renderer,light,cube;
 
     hexagon.size = function(_) {
         if (!arguments.length) return size;
@@ -11,11 +13,72 @@ hexagon = function(){
         return hexagon;
     };
 
-    hexagon.init = function() {
-        console.log('this is my log');
+    hexagon.init = function(canvasContainer) {
+        console.log(canvasContainer);
 
+        // init three
+        width = size[0];
+        height = size[1];
+
+       // console.log(width);
+       // console.log(height);
+
+        renderer = new THREE.WebGLRenderer({
+            antialias : true
+        });
+
+        renderer.setSize(width, height);
+        document.getElementById(canvasContainer).appendChild(renderer.domElement);
+        renderer.setClearColor(0xFFFFFF, 1.0);
+
+        // init camera
+        camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+        camera.position.x = 0;
+        camera.position.y = 1000;
+        camera.position.z = 0;
+        camera.up.x = 0;
+        camera.up.y = 0;
+        camera.up.z = 1;
+        camera.lookAt({
+            x : 0,
+            y : 0,
+            z : 0
+        });
+
+        // init scene
+        scene = new THREE.Scene();
+
+        // init light
+        light = new THREE.DirectionalLight(0xFF0000, 1.0, 0);
+        light.position.set(100, 100, 200);
+        scene.add(light);
+
+        // init object
+        var geometry = new THREE.Geometry();
+        geometry.vertices.push( new THREE.Vector3( - 500, 0, 0 ) );
+        geometry.vertices.push( new THREE.Vector3( 500, 0, 0 ) );
+
+        for ( var i = 0; i <= 20; i ++ ) {
+
+            var line = new THREE.Line( geometry, new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2 } ) );
+            line.position.z = ( i * 50 ) - 500;
+            scene.add( line );
+
+            var line = new THREE.Line( geometry, new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2 } ) );
+            line.position.x = ( i * 50 ) - 500;
+            line.rotation.y = 90 * Math.PI / 180;
+            scene.add( line );
+
+        }
         return hexagon;
     }
+
+    hexagon.render = function(){
+        renderer.clear();
+        renderer.render(scene, camera);
+    }
+
+
 
     return hexagon;
 }();
